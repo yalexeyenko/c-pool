131a531ea09832ff56d76ff1c615b96d51996d22
Yevgeniy Alexeyenko <yevgeniy_alexeyenko@epam.com>
2017-04-07 16:53:52 +0600

    Create ConnectionPoolTest test class.

---
+++ src/test/java/ConnectionPoolTest.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Connection;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Yevgeniy_Alexeyenko on 4/7/2017.
+ */
+public class ConnectionPoolTest {
+    private ConnectionPool pool;
+
+    @Before
+    public void init() {
+        this.pool = new ConnectionPool(10);
+    }
+
+    @Test
+    public void shouldCreateConnectionPoolOfSizeTen() {
+        assertEquals(10, pool.getMaxSize());
+    }
+
+    @Test
+    public void shouldTakeOneConnectionFromThePool() {
+        ProxyConnection conn = pool.getConnection();
+        assertNotEquals(null, conn);
+        assertEquals(9, pool.getMaxSize());
+
+        pool.returnConnection(conn);
+        assertEquals(10, pool.getMaxSize());
+    }
+}
